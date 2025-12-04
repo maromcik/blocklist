7e2c8209c5c079370adc866ef3d8ab4ea2858193
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2025-12-04 23:32:11 +0100

    insert data

--- src/handlers/blocklist.rs
+++ src/handlers/blocklist.rs
@@ -12,22 +12,63 @@ use diesel::{QueryDsl, SelectableHelper};
 use diesel_async::RunQueryDsl;
 use ipnetwork::IpNetwork;
 use itertools::Itertools;
+use log::info;
+use tokio::fs;
+use tracing::error;
+
+pub async fn read_ip_set_file(path: &str) -> Result<String, AppError> {
+
+            let data = fs::read_to_string(path).await.unwrap();
+    Ok(data)
+}
+
+pub fn parse_from_string<S: AsRef<str>>(
+    data: Option<S>,
+    split_string: Option<&str>,
+) -> Option<Vec<String>> {
+    match data {
+        Some(s) if !s.as_ref().is_empty() => match split_string {
+            None => Some(
+                s.as_ref()
+                    .split_whitespace()
+                    .map(|s| s.trim().to_string())
+                    .collect(),
+            ),
+            Some(split_str) => Some(
+                s.as_ref()
+                    .split(split_str)
+                    .map(|s| s.trim().to_string())
+                    .collect(),
+            ),
+        },
+        _ => None,
+    }
+}
 
 pub async fn get_ip(
     DbConnection(mut conn): DbConnection,
     params: Query<BlocklistIpVersion>,
 ) -> Result<impl IntoResponse, AppError> {
-    let ips = blocklist::table
-        .select(Blocklist::as_select())
-        .filter(blocklist::version.eq(params.ip_version))
-        .load::<Blocklist>(&mut conn)
-        .await
-        .map_err(DbError::from)?
+    let ips= parse_from_string(Some(read_ip_set_file("blist.txt").await.expect("NOT FOUND")), None).expect("PARSE ERROR");
+    let items: Vec<BlocklistCreate> = ips
         .into_iter()
-        .map(|ip|ip.ip)
-        .join("\n");
-    Ok(ips)
-}
+        .map(|ip_str| BlocklistCreate {
+            ip: ip_str.parse::<IpNetwork>().unwrap(), // or ? for error
+            version: IpVersion::Ipv4,
+            description: None,
+        })
+        .collect();
+    let inserted = diesel::insert_into(blocklist::table)
+        .values(&items)
+        .on_conflict(blocklist::ip)       // ← ignore duplicates
+        .do_nothing()
+        .execute(&mut conn)
+        .await
+        .map_err(DbError::from)?;
+    Ok("success")
+
+    }
+
 
 pub async fn add_ip(
     DbConnection(mut conn): DbConnection,
